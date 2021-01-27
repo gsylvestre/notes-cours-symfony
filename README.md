802068336adf48ec0f5695f3bbe7ddaa6783ea9f
Guillaume <gsylvestre@gmail.com>
2021-01-27 09:13:24 +0100

    mise en place

---
+++ README.md
@@ -0,0 +1,7 @@
+### Instructions d'installation
+
+```
+git clone https://github.com/gsylvestre/leboncoin-eni.git  
+cd leboncoin-eni  
+composer install
+```
---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_home")
+     */
+    public function home()
+    {
+        return $this->render('main/home.html.twig', []);
+    }
+
+    /**
+     * @Route("/test", name="main_test")
+     */
+    public function test()
+    {
+        $username = "<h1>jean</h1>";
+        return $this->render('main/test.html.twig', [
+            "username" => $username,
+            "product" => "pifpaf"
+        ]);
+    }
+}
